b60a45fe2c45f5cbe2487254737a386c4bae0285
Shen yean <51376621+Shenyean@users.noreply.github.com>
2020-03-19 11:36:11 +0800

    Add files via upload

---
+++ NotificationMicroservice/NotificationMicroservice/Controllers/NotificationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        // GET: api/Notification
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Notification/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Notification
+        [HttpPost]
+        public IActionResult Post(string studentName, int studentId)
+        {
+            Debug.WriteLine("Email to {0} with studentId {1} for payment", studentName, studentId);
+            return Ok();
+        }
+
+        // PUT: api/Notification/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
